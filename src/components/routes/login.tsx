69b5ee49f843d63047966dd8da09ac19cdc7a7e9
wojtekzielinski96@gmail.com <wojtekzielinski96@gmail.com>
2026-02-27 23:04:37 +0100

    implement initial login form with mocked authentication * TODO add styling to this form * TODO implement protected and public only route wrapper

--- src/components/routes/login.tsx
+++ src/components/routes/login.tsx
@@ -1,19 +1,55 @@
 import { MobileLayout } from "@components/layout";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 
 export const Login = () => {
   const navigate = useNavigate();
 
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e: React.SubmitEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (email === "wz" && password === "123") {
+      navigate("/transactions")
+    }
+    else {
+      alert("invalid credentials")
+    }
+
+  }
+
+
   return (
     <MobileLayout>
       <h1>Login will be here</h1>
-      <button
-        className="border border-fg py-2 px-4 rounded-xl mt-1 bg-bg hover:bg-fg/20 cursor-pointer"
-        onClick={() => { navigate("/transactions") }}
-      >
-        Navigate to transactions
-      </button>
+
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">Email</label>
+        <input
+          id="email"
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+          placeholder="Email"
+        />
+        
+        <label htmlFor="password">Password</label>
+        <input
+          id="password"
+          type="password"
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+          placeholder="Password"
+        />
+
+        <button type="submit">Login</button>
+
+      </form>
+
+
+
     </MobileLayout>
   )
 }
