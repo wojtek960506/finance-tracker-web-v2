8025fe31427aea8fd5e300e66ec411b1f5bb45a5
wojtekzielinski96@gmail.com <wojtekzielinski96@gmail.com>
2026-03-23 00:03:25 +0100

    test: set up vitest and add basic app render test
    - add vitest + RTL with v8 coverage and config split
    - add vitest tsconfig + editor types for globals
    - add setupTests and very trivial test
    - align versions and add tsconfig paths plugin

---
+++ src/app/App.test.tsx
@@ -0,0 +1,7 @@
+import { describe, expect, it } from 'vitest';
+
+describe('very basic just to try', () => {
+  it('adds two numbers', () => {
+    expect(2 + 2).toBe(4);
+  });
+});
---
+++ vitest.config.ts
@@ -0,0 +1,30 @@
+import tsConfigPaths from 'vite-tsconfig-paths';
+import { defineConfig, mergeConfig } from 'vitest/config';
+
+import viteConfig from './vite.config';
+
+export default mergeConfig(
+  viteConfig,
+  defineConfig({
+    plugins: [tsConfigPaths()],
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      includeSource: ['src/**/*.{ts,tsx}'],
+      include: ['src/**/*.test.{ts,tsx}', 'src/**/*.spec.{ts,tsx}'],
+      setupFiles: './src/setupTests.ts',
+      coverage: {
+        provider: 'v8',
+        reporter: ['text', 'html'],
+        reportsDirectory: './coverage',
+        include: ['src/**/*.{ts,tsx}'],
+        exclude: [
+          'src/**/*.test.{ts,tsx}',
+          'src/**/*.spec.{ts,tsx}',
+          'src/**/index.ts',
+          'src/**/*types*.ts',
+        ],
+      },
+    },
+  }),
+);
