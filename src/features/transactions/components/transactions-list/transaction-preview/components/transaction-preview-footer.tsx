c4bd101298653233dfaa2d6da024fde410b6cdbd
wojtekzielinski96@gmail.com <wojtekzielinski96@gmail.com>
2026-09-16 19:47:03 +0200

    refactor: extract transaction preview sub-components into dedicated files

---
+++ src/features/transactions/components/transactions-list/transaction-preview/components/transaction-preview-footer.tsx
@@ -0,0 +1,44 @@
+import { useTranslation } from 'react-i18next';
+
+import type { Transaction, TrashedTransaction } from '@transactions/api';
+import { getTransactionNamedResourceLabel } from '@transactions/utils';
+import { ButtonLink } from '@ui';
+
+type TransactionPreviewFooterProps = {
+  transaction: Transaction | TrashedTransaction;
+};
+
+const GHOST_LINK_CLASS_NAME = 'text-sm sm:text-base';
+
+export const TransactionPreviewFooter = ({
+  transaction,
+}: TransactionPreviewFooterProps) => {
+  const { t: tNamedResources } = useTranslation('namedResources');
+
+  const accountLabel = getTransactionNamedResourceLabel(
+    transaction.account,
+    tNamedResources,
+  );
+  const paymentMethodLabel = getTransactionNamedResourceLabel(
+    transaction.paymentMethod,
+    tNamedResources,
+  );
+  const categoryLabel = getTransactionNamedResourceLabel(
+    transaction.category,
+    tNamedResources,
+  );
+
+  return (
+    <footer className="flex flex-wrap items-center justify-between justify-end gap-1">
+      <ButtonLink to="/accounts" className={GHOST_LINK_CLASS_NAME}>
+        {accountLabel}
+      </ButtonLink>
+      <ButtonLink to="/paymentMethods" className={GHOST_LINK_CLASS_NAME}>
+        {paymentMethodLabel}
+      </ButtonLink>
+      <ButtonLink to="/categories" className={GHOST_LINK_CLASS_NAME}>
+        {categoryLabel}
+      </ButtonLink>
+    </footer>
+  );
+};
